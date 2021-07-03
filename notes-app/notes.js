027002f0ce8e59d8127f95eb2af156744724be10
Freeman-md <freemansparda@gmail.com>
2021-07-03 22:04:34 +0100

    Removing a note

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const chalk = require('chalk')
 
 const getNotes = function() {
   return 'Your notes...'
@@ -23,6 +24,27 @@ const addNote = function(title, body) {
   }
 }
 
+const removeNote = function(title) {
+  const notes = loadNotes()
+
+  const notesToKeep = notes.filter(function(note) {
+    return note.title !== title
+  })
+
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse('Note removed!'))
+    saveNotes(notesToKeep)
+  } else {
+    console.log(chalk.red.inverse('No note found!'))
+  }
+
+}
+
+const saveNotes = function(notes) {
+  const dataJSON = JSON.stringify(notes)
+  fs.writeFileSync('notes.json', dataJSON)
+}
+
 const loadNotes = function() {
   try {
     const dataBuffer = fs.readFileSync('notes.json')
@@ -33,12 +55,8 @@ const loadNotes = function() {
   }
 }
 
-const saveNotes = function(notes) {
-  const dataJSON = JSON.stringify(notes)
-  fs.writeFileSync('notes.json', dataJSON)
-}
-
 module.exports = {
   getNotes: getNotes, 
-  addNote: addNote
+  addNote: addNote,
+  removeNote: removeNote
 }
