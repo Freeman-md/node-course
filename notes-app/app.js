214401a1a1464cb30a5f175cca18a92e638ee1d9
Freeman-md <freemansparda@gmail.com>
2021-07-03 17:27:14 +0100

    Getting Input from Users

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,12 @@
 const chalk = require('chalk')
+const getNotes = require('./notes.js')
 
-const getNotes = require('./notes')
+const command = process.argv[2]
 
-const msg = getNotes()
+console.log(process.argv)
 
-console.log(chalk.bgGreen('Success!'))
+if (command === 'add') {
+  console.log('Adding note!')
+} else if (command === 'remove') {
+  console.log('Removing note!')
+}
