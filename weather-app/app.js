8c21f40129906cc4e8aae0b9da7274875b396f51
Freeman-md <freemanmadudili@gmail.com>
2021-07-08 08:54:30 +0100

    Customizing HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=fceea1ad480ddb891cfca3288ae6242d'
+// const url = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=fceea1ad480ddb891cfca3288ae6242d'
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&units=imperial&appid=fceea1ad480ddb891cfca3288ae6242d'
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body)
-  console.log(data.main)
+request({ url: url, json: true }, (error, response) => {
+  console.log('It is currently ' + response.body.main.temp + ' degrees out.')
 })
